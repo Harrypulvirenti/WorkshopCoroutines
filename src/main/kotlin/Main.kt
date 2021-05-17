4d62a0860ba95b774f78ade7d13eac351124f2b7
Harry Pulvirenti <>
2021-05-17 17:56:56 +0200

    suspend functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,26 @@
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking{
-    GlobalScope.launch {
-        println("Hello Coroutines!")
+fun main() = runBlocking {
+    launch {
+        val user = async { fetchUser() }
+
+        println("Inside Launch | Thread ${Thread.currentThread()}")
+
+        println("User: ${user.await().name}")
     }.join()
 }
+
+suspend fun fetchUser(): User {
+    println("Fetch User | Thread ${Thread.currentThread()}")
+
+    delay(2000)
+
+    println("Delay Finished | Thread ${Thread.currentThread()}")
+
+    return User("Enrico")
+}
+
+data class User(val name: String)

a04480fb84155a641ea0c1ad4cb21693870da08d
enrico.pulvirenti <enrico.pulvirenti@n26.com>
2021-07-08 10:02:34 +0200

    refactor exercises

---
+++ src/main/kotlin/suspend/Exercise1.kt
@@ -0,0 +1,43 @@
+package suspend
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch {
+        Example1.solve(
+            { fakeNetworkCall("Call 1") },
+            { fakeNetworkCall("Call 2") },
+            { fakeNetworkCall("Call 3") },
+        )
+    }.join()
+}
+
+object Example1 {
+
+    /**
+     * Implement a function that execute all the provided network calls in row like a concat operator
+     *
+     * Expected output:
+     *
+     * Starting Network Call Call 1
+     * Network Call Call 1 executed
+     *
+     * Starting Network Call Call 2
+     * Network Call Call 2 executed
+     *
+     * Starting Network Call Call 3
+     * Network Call Call 3 executed
+     */
+    suspend fun solve(vararg calls: suspend () -> Unit) {
+
+    }
+}
+
+/**
+ * Do not change this function
+ */
+private suspend fun fakeNetworkCall(fakeName: String) {
+    println("Starting Network Call $fakeName")
+    delay(2000)
+    println("Network Call $fakeName executed")
+}
---
+++ src/main/kotlin/suspend/Exercise2.kt
@@ -0,0 +1,43 @@
+package suspend
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch {
+        Example2.solve(
+            { fakeNetworkCall("Call 1") },
+            { fakeNetworkCall("Call 2") },
+            { fakeNetworkCall("Call 3") },
+        )
+    }.join()
+}
+
+object Example2 {
+
+    /**
+     * Implement a function that execute all the provided network calls at the same times like a zip operator
+     *
+     * Expected output:
+     *
+     * Starting Network Call Call 1
+     * Starting Network Call Call 2
+     * Starting Network Call Call 3
+     *
+     * Network Call Call 1 executed
+     * Network Call Call 2 executed
+     * Network Call Call 3 executed
+     *
+     */
+    suspend fun solve(vararg calls: suspend () -> Unit) {
+
+    }
+}
+
+/**
+ * Do not change this function
+ */
+private suspend fun fakeNetworkCall(fakeName: String) {
+    println("Starting Network Call $fakeName")
+    delay(2000)
+    println("Network Call $fakeName executed")
+}
